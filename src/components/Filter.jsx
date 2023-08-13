f6cf87a323a0617f5e3ef8119e2879590e042e89
SwapnilV26 <swapnilvish2812@gmail.com>
2023-08-13 17:21:39 +0530

    feat: Header with filer added

---
+++ src/components/Filter.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const Filter = () => {
+    return (
+        <div className='bg-gray-50 w-72 ml-10 rounded-lg p-5 absolute -mt-2 border border-gray-200 shadow-md'>
+            <div className='flex justify-between'>
+                <span className='text-gray-400 font-medium'>Grouping</span>
+                <select id="small" className="p-1 pr-5 text-sm font-medium cursor-pointer text-gray-900 border border-gray-300 rounded-md bg-gray-50">
+                    <option selected value="status">Status</option>
+                    <option value="user">User</option>
+                    <option value="priority">Priority</option>
+                </select>
+            </div>
+            <div className='flex justify-between mt-2'>
+                <span className='text-gray-400 font-medium'>Ordering</span>
+                <select id="small" className="p-1 pr-5 text-sm font-medium cursor-pointer text-gray-900 border border-gray-300 rounded-md bg-gray-50">
+                    <option value="title">Title</option>
+                    <option selected value="priority">Priority</option>
+                </select>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+        <div className='bg-gray-100 h-screen text-center'>Home Content</div>
+    </div>
+  )
+}
+
+export default Home
